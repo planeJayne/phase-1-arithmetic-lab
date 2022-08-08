761737625197236c2b2fcb5f3628a8b267fb8ede
Jasmin <jasmin.parish@outlook.com>
2022-08-08 19:58:24 -0400

    commit tree 966b5bdcdbb4fd5cdb753b235179ccb4857b4739

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+console.log(multiply)
+
+let random = Math.ceil(Math.random()*10)
+console.log(random)
+
+
+let num3 = 32
+let num4 = 8
+let mod = num3/num4
+console.log(mod)
+
+let max = Math.max(5, 10, 15, 20)
+console.log(max)
